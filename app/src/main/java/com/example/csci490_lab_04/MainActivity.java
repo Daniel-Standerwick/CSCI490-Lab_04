741b43ae24636f3e29bd4de18c84edae860beffc
Daniel Standerwick <standerwickd@gmail.com>
2019-03-04 18:16:24 -0500

    Connected to github

---
+++ app/src/main/java/com/example/csci490_lab_04/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.csci490_lab_04;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+        WebView webview = new WebView (this);
+        setContentView(webview);
+        webview.loadUrl ("https://github.com/Daniel-Standerwick");
+
+    }
+}
